07d4836a5e54ef5ebe6f505ea383f8c833d90d76
Goutam Singh <java.lang.goutam@gmail.com>
2024-02-23 04:38:06 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (61.43%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -1,17 +1,20 @@
 class Solution {
 
     public int findKthPositive(int[] arr, int k) {
-        int prev = 0;
-        for (final int ele : arr) {
-            final int diff = ele - prev - 1;
-            k -= diff;
-            if (k > 0) {
-                prev = ele;
-            } else {
-                return k + ele - 1;
-            }
+        int low = 0, high = arr.length;
+
+        // find the first element whose diff with actual is greater than k
+        while (low < high) {
+            final int mid = low + ((high - low) >> 1);
+            final int diffElement = arr[mid] - (mid + 1);
+            if (diffElement < k) low = mid + 1; else high = mid;
         }
 
-        return prev + k;
+        // how much the next greater element is more than k ?
+        // we know the position res = arr[high] - (diff[high] - k + 1) = arr[high] - diff[high] + k - 1
+        // diff[high] = arr[high] - (high + 1) =  arr[high] - high - 1
+        // res = arr[high] - arr[high] + high + 1 + k - 1 = high + k
+
+        return high + k;
     }
 }
