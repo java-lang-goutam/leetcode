fb90b994684ca1d254e3b4b8f4542f73cb388ad1
Goutam Singh <java.lang.goutam@gmail.com>
2024-03-09 13:16:54 +0530

    Time: 4 ms (38.22%), Space: 62.4 MB (80.15%) - LeetHub

---
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+        
+        final int[] smaller, bigger;
+        
+        if (nums1.length < nums2.length) {
+            smaller = nums1;
+            bigger = nums2;
+        } else {
+            smaller = nums2;
+            bigger = nums1;
+        }
+        
+        for (int ele : smaller) {
+            if (exists(bigger, ele)) return ele;
+        }
+        
+        return -1;
+    }
+    
+    public boolean exists(final int[] nums, final int ele) {
+        
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
+            final int mid = low + ((high - low) >> 1);
+            final int middleEle = nums[mid];
+            if (middleEle == ele) return true;
+            if (middleEle < ele) low = mid + 1;
+            else high = mid - 1;
+        }
+        
+        return false;
+    }
+}
