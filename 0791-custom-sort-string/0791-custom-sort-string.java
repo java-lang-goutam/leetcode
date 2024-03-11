888829ae5689ff50d467f963bd98167097b55dde
Goutam Singh <java.lang.goutam@gmail.com>
2024-03-11 21:51:34 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (85.81%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.java
+++ 0791-custom-sort-string/0791-custom-sort-string.java
@@ -1,18 +1,31 @@
 class Solution {
 
     public String customSortString(String order, String s) {
-        final PriorityQueue<Character> pq = new PriorityQueue<>(
-            (c1, c2) -> {
-                return order.indexOf(c1) - order.indexOf(c2);
-            }
-        );
+        final int[] count = new int[26];
 
         for (int i = 0, length = s.length(); i < length; i++) {
-            pq.offer(s.charAt(i));
+            count[s.charAt(i) - 'a']++;
         }
 
         final StringBuilder sb = new StringBuilder();
-        while (!pq.isEmpty()) sb.append(pq.poll());
+        for (int i = 0, length = order.length(); i < length; i++) {
+            final char c = order.charAt(i);
+            final int index = c - 'a';
+            for (int j = 0, total = count[index]; j < total; j++) {
+                sb.append(c);
+            }
+            count[index] = 0;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            final int total = count[i];
+            if (total > 0) {
+                final char c = (char) ('a' + i);
+                for (int j = 0; j < total; j++) {
+                    sb.append(c);
+                }
+            }
+        }
 
         return sb.toString();
     }
