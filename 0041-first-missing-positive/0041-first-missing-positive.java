0c547c6f5509748f02ca1deecf3effb0a9def669
Goutam Singh <java.lang.goutam@gmail.com>
2024-03-26 17:40:56 +0530

    Time: 1 ms (98.00%), Space: 55.1 MB (51.79%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,25 +1,29 @@
 class Solution {
 
+    /**
+        Strategy is to mark the occurance/index as negative so that in next iteration we will know which number is missing    
+    */
     public int firstMissingPositive(int[] nums) {
         final int size = nums.length;
         final int maxVal = 1_000_000;
 
+        // remove all negatives and zeros
         for (int i = 0; i < size; i++) {
             if (nums[i] <= 0) nums[i] = maxVal;
         }
 
+        // mark all valid index as negative
         for (int i = 0; i < size; i++) {
-            // System.out.println(Arrays.toString(nums));
-            final int index = Math.abs(nums[i]);
-            if (index > size) continue;
-            if (nums[index-1] > 0) nums[index - 1] *= -1;
+            final int index = Math.abs(nums[i]) - 1;
+            if (index >= size) continue;
+            if (nums[index] > 0) nums[index] *= -1;
         }
 
-        //System.out.println(Arrays.toString(nums));
-
+        // find first positive element
         int i = 0;
-        for (; i < size; i++) {
-            if (nums[i] >= 0) return i + 1;
+        while (i < size) {
+            if (nums[i] >= 0) break;
+            i++;
         }
 
         return i + 1;
