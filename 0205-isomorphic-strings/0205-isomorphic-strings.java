11f12e48bfffa7a4f6c75fedffcc13b2e4d852c6
Goutam Singh <java.lang.goutam@gmail.com>
2024-04-02 07:27:31 +0530

    Time: 4 ms (98.36%), Space: 42.6 MB (26.81%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,21 +1,24 @@
 class Solution {
-    
-    public boolean isIsomorphic(String s, String t) {
-        return verify(s, t) && verify(t, s);
-    }
 
-    private boolean verify(String s, String t) {
+    public boolean isIsomorphic(String s, String t) {
         final char[] arr = new char[95];
+        final boolean[] visited = new boolean[95];
 
         for (int i = 0, n = s.length(); i < n; i++) {
             final int arrIndex = s.charAt(i) - ' ';
             final char arrVal = arr[arrIndex];
+            final char tVal = t.charAt(i);
 
             if (arrVal != '\0') {
-                if (arrVal != t.charAt(i)) return false;
+                if (arrVal != tVal) return false;
             } else {
-                arr[arrIndex] = t.charAt(i);
+                final int tIndex = tVal - ' ';
+                if (visited[tIndex]) return false; else {
+                    arr[arrIndex] = tVal;
+                    visited[tIndex] = true;
+                }
             }
+            //System.out.println(Arrays.toString(arr));
         }
 
         return true;
