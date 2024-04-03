1902a946e1e832a19d05503d53cec694c309d221
Goutam Singh <java.lang.goutam@gmail.com>
2024-04-03 06:27:00 +0530

    Time: 72 ms (96.19%), Space: 41.4 MB (61.18%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -13,36 +13,36 @@ class Solution {
 
         for (int i = 0; i < totalRows; i++) {
             for (int j = 0; j < totalCols; j++) {
-                if (board[i][j] == word.charAt(0) && dfs(i, j, 0)) return true;
+                if (board[i][j] == word.charAt(0) && dfs(i, j, 0, included, board, word)) return true;
             }
         }
 
         return false;
     }
 
-    private boolean dfs(int i, int j, int index) {
+    private boolean dfs(int i, int j, int index, boolean[][] included, char[][] board, String word) {
         if (index == word.length() - 1) return true;
 
         included[i][j] = true;
         final char nextChar = word.charAt(index + 1);
 
         // up check
-        if (i > 0 && board[i - 1][j] == nextChar && !included[i - 1][j] && dfs(i - 1, j, index + 1)) {
+        if (i > 0 && board[i - 1][j] == nextChar && !included[i - 1][j] && dfs(i - 1, j, index + 1, included, board, word)) {
             return true;
         }
 
         // down check
-        if (i < board.length - 1 && board[i + 1][j] == nextChar && !included[i + 1][j] && dfs(i + 1, j, index + 1)) {
+        if (i < board.length - 1 && board[i + 1][j] == nextChar && !included[i + 1][j] && dfs(i + 1, j, index + 1, included, board, word)) {
             return true;
         }
 
         // left check
-        if (j > 0 && board[i][j - 1] == nextChar && !included[i][j - 1] && dfs(i, j - 1, index + 1)) {
+        if (j > 0 && board[i][j - 1] == nextChar && !included[i][j - 1] && dfs(i, j - 1, index + 1, included, board, word)) {
             return true;
         }
 
         // right check
-        if (j < board[0].length - 1 && board[i][j + 1] == nextChar && !included[i][j + 1] && dfs(i, j + 1, index + 1)) {
+        if (j < board[0].length - 1 && board[i][j + 1] == nextChar && !included[i][j + 1] && dfs(i, j + 1, index + 1, included, board, word)) {
             return true;
         }
         
