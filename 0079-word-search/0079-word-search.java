63ad9f4ae54cc9bbd4d0b4d3e972efeb9dd52317
Goutam Singh <java.lang.goutam@gmail.com>
2024-04-03 06:42:00 +0530

    Time: 76 ms (96.07%), Space: 41.2 MB (84.84%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -3,40 +3,41 @@ class Solution {
     public boolean exist(char[][] board, String word) {
         final int totalRows = board.length;
         final int totalCols = board[0].length;
+
         final boolean[][] included = new boolean[totalRows][totalCols];
 
         for (int i = 0; i < totalRows; i++) {
             for (int j = 0; j < totalCols; j++) {
-                if (board[i][j] == word.charAt(0) && dfs(i, j, 0, included, board, word, totalRows, totalCols)) return true;
+                if (board[i][j] == word.charAt(0) && dfs(i, j, 0, included, board, word)) return true;
             }
         }
 
         return false;
     }
 
-    private boolean dfs(int i, int j, int index, boolean[][] included, char[][] board, String word, int totalRows, int totalCols) {
+    private boolean dfs(int i, int j, int index, boolean[][] included, char[][] board, String word) {
         if (index == word.length() - 1) return true;
 
         included[i][j] = true;
         final char nextChar = word.charAt(index + 1);
 
         // up check
-        if (i > 0 && board[i - 1][j] == nextChar && !included[i - 1][j] && dfs(i - 1, j, index + 1, included, board, word, totalRows, totalCols)) {
+        if (i > 0 && board[i - 1][j] == nextChar && !included[i - 1][j] && dfs(i - 1, j, index + 1, included, board, word)) {
             return true;
         }
 
         // down check
-        if (i < totalRows - 1 && board[i + 1][j] == nextChar && !included[i + 1][j] && dfs(i + 1, j, index + 1, included, board, word, totalRows, totalCols)) {
+        if (i < board.length - 1 && board[i + 1][j] == nextChar && !included[i + 1][j] && dfs(i + 1, j, index + 1, included, board, word)) {
             return true;
         }
 
         // left check
-        if (j > 0 && board[i][j - 1] == nextChar && !included[i][j - 1] && dfs(i, j - 1, index + 1, included, board, word, totalRows, totalCols)) {
+        if (j > 0 && board[i][j - 1] == nextChar && !included[i][j - 1] && dfs(i, j - 1, index + 1, included, board, word)) {
             return true;
         }
 
         // right check
-        if (j < totalCols - 1 && board[i][j + 1] == nextChar && !included[i][j + 1] && dfs(i, j + 1, index + 1, included, board, word, totalRows, totalCols)) {
+        if (j < board[0].length - 1 && board[i][j + 1] == nextChar && !included[i][j + 1] && dfs(i, j + 1, index + 1, included, board, word)) {
             return true;
         }
         
