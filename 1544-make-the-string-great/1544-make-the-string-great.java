5783385527f05295c355cc3c66c4da66517e0243
Goutam Singh <java.lang.goutam@gmail.com>
2024-04-05 09:28:33 +0530

    Time: 1 ms (100.00%), Space: 41.5 MB (97.94%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.java
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -1,12 +1,11 @@
 class Solution {
 
     private boolean check(final int c1, final int c2) {
-        final int diff = c1 - c2;
-        return diff == 32 || diff == -32;
+        return (c2 + 32) == c1 || (c1 + 32) == c2;
     }
 
     public String makeGood(String s) {
-        final StringBuilder sb = new StringBuilder(s.length() + 1);
+        final StringBuilder sb = new StringBuilder(s.length());
         for (int i = 0, length = s.length(); i < length; i++) {
             final char c = s.charAt(i);
             final int lastIndex = sb.length() - 1;
