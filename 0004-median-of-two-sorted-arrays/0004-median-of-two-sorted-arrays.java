618e38de637cceacd02b8a85a070c90ac03452a1
Goutam Singh <java.lang.goutam@gmail.com>
2024-04-05 12:29:58 +0530

    Time: 1 ms (100.00%), Space: 46 MB (51.48%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,5 +1,8 @@
 class Solution {
 
+    /**
+    Details - https://www.youtube.com/watch?v=LPFhl65R7ww
+    */
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         final int x = nums1.length;
         final int y = nums2.length;
@@ -10,8 +13,6 @@ class Solution {
         final int paritionSize = ((totalLength + 1) >> 1);
         final int INF = Integer.MAX_VALUE;
         final int NEG_INF = Integer.MIN_VALUE;
-
-        //System.out.println("paritionSize : " + paritionSize);
 
         int low = 0, high = x;
 
@@ -26,15 +27,9 @@ class Solution {
             final int minRightY = partitionY == y ? INF : nums2[partitionY];
 
             if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
-                // System.out.println("maxLeftX : " + maxLeftX);
-                // System.out.println("minRightX : " + minRightX);
-                // System.out.println("maxLeftY : " + maxLeftY);
-                // System.out.println("minRightY : " + minRightY);
-
                 if ((totalLength & 1) == 1) {
                     return (double) Math.max(maxLeftX, maxLeftY);
                 } else {
-                    // System.out.println("odd : " + false);
                     return (Math.max(maxLeftX, maxLeftY) + Math.min(minRightX, minRightY)) / 2.0;
                 }
             } else if (maxLeftX > minRightY) {
