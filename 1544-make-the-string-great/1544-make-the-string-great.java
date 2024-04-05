37b008b3837b60bfdf19e28c8c61e1d303ffe594
Goutam Singh <java.lang.goutam@gmail.com>
2024-04-05 08:33:48 +0530

    Time: 3 ms (61.07%), Space: 42.1 MB (57.36%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    private boolean check(final char c1, final char c2) {
+        if (Character.isUpperCase(c2)) {
+            return Character.toLowerCase(c2) == c1;
+        } else if (Character.isUpperCase(c1)) {
+            return Character.toLowerCase(c1) == c2;
+        }
+
+        return false;
+    }
+
+    public String makeGood(String s) {
+        final Stack<Character> stack = new Stack<>();
+        stack.push(s.charAt(0));
+
+        for (int i = 1, length = s.length(); i < length; i++) {
+            final char c = s.charAt(i);
+            if (stack.isEmpty() || !check(stack.peek(), c)) stack.push(c); else stack.pop();
+        }
+
+        final StringBuilder sb = new StringBuilder(stack.size());
+        for (char c : stack) {
+            sb.append(c);
+        }
+
+        return sb.toString();
+    }
+}
