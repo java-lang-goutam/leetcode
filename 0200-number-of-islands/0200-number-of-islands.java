51861cdb49c9d48562e66214256de11e1cc4a5bc
Goutam Singh <java.lang.goutam@gmail.com>
2024-04-19 11:51:51 +0530

    Time: 2 ms (99.65%), Space: 49.4 MB (57.87%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,55 +1,29 @@
 class Solution {
+
     public int numIslands(char[][] grid) {
-        
-        final char land = '1';
-        int islandCount = 0;
-        for (int r=0, rn = grid.length; r < rn; r++) {
-            for (int c=0, cn = grid[0].length; c < cn; c++) {
-                if (grid[r][c] == land) {
-                    bfs(r, c, grid);
-                    islandCount++;
+        final int m = grid.length;
+        final int n = grid[0].length;
+
+        int noOfIslands = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    searchIsland(grid, i, j);
+                    noOfIslands++;
                 }
             }
         }
-        
-        return islandCount;
+
+        return noOfIslands;
     }
-    
-    private void bfs(final int row, final int col, final char[][] grid) {
+
+    private void searchIsland(final char[][] grid, final int r, final int c) {
         
-        final Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{row, col});
-        
-        final int maxRowIndex = grid.length - 1;
-        final int maxColIndex = grid[0].length - 1;
-        final char land = '1';
-        final char water = '0';
-        
-        grid[row][col] = water;
-        
-        while (!queue.isEmpty()) {
-            final int[] rc = queue.poll();
-            final int r = rc[0];
-            final int c = rc[1];
-            final int nextRow = r + 1, prevRow = r - 1;
-            final int nextCol = c + 1, prevCol = c - 1;
-            
-            if (r > 0 && grid[prevRow][c] == land) {
-                grid[prevRow][c] = water;
-                queue.offer(new int[]{prevRow, c});
-            } 
-            if (r < maxRowIndex && grid[nextRow][c] == land) {
-                grid[nextRow][c] = water;
-                queue.offer(new int[]{nextRow, c});
-            }
-            if (c > 0 && grid[r][prevCol] == land) {
-                grid[r][prevCol] = water;
-                queue.offer(new int[]{r, prevCol});
-            } 
-            if (c < maxColIndex && grid[r][nextCol] == land) {
-                grid[r][nextCol] = water;
-                queue.offer(new int[]{r, nextCol});
-            } 
-        }
+        grid[r][c] = 0;
+
+        if (c + 1 < grid[0].length && grid[r][c + 1] == '1') searchIsland(grid, r, c + 1);
+        if (r + 1 < grid.length && grid[r + 1][c] == '1') searchIsland(grid, r + 1, c);
+        if (c > 0 && grid[r][c - 1] == '1') searchIsland(grid, r, c - 1);
+        if (r > 0 && grid[r - 1][c] == '1') searchIsland(grid, r - 1, c);
     }
 }
