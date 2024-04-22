dd60910b7a3180934cce872b7b84a439d94f03ec
Goutam Singh <java.lang.goutam@gmail.com>
2024-04-22 07:06:07 +0530

    Time: 77 ms (87.26%), Space: 46.5 MB (81.74%) - LeetHub

--- 0752-open-the-lock/0752-open-the-lock.java
+++ 0752-open-the-lock/0752-open-the-lock.java
@@ -1,57 +1,13 @@
 class Solution {
 
     private char getNextChar(final char c) {
-        switch (c) {
-            case '0':
-                return '1';
-            case '1':
-                return '2';
-            case '2':
-                return '3';
-            case '3':
-                return '4';
-            case '4':
-                return '5';
-            case '5':
-                return '6';
-            case '6':
-                return '7';
-            case '7':
-                return '8';
-            case '8':
-                return '9';
-            case '9':
-                return '0';
-        }
-
-        return '\0';
+        if (c == '9') return '0';
+        return (char) (c + 1);
     }
 
     private char getPrevChar(final char c) {
-        switch (c) {
-            case '0':
-                return '9';
-            case '1':
-                return '0';
-            case '2':
-                return '1';
-            case '3':
-                return '2';
-            case '4':
-                return '3';
-            case '5':
-                return '4';
-            case '6':
-                return '5';
-            case '7':
-                return '6';
-            case '8':
-                return '7';
-            case '9':
-                return '8';
-        }
-
-        return '\0';
+        if (c == '0') return '9';
+        return (char) (c - 1);
     }
 
     public int openLock(String[] deadends, String target) {
