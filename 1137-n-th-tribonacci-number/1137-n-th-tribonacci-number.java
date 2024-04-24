457876faef906332ba950082cd5ffe0595593870
Goutam Singh <java.lang.goutam@gmail.com>
2024-04-24 17:14:23 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (73.08%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -2,8 +2,7 @@ class Solution {
 
     public int tribonacci(int n) {
 
-        if (n == 0) return 0;
-        if (n == 1 || n == 2) return 1;
+        if (n < 2) return n;
         
         int t0 = 0, t1 = 1, t2 = 1, tn = 0;
         
@@ -14,6 +13,6 @@ class Solution {
             t2 = tn;
         }
 
-        return tn;
+        return t2;
     }
 }
