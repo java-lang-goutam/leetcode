91a327ace011c98ba41f2026d36ef0244dbfeadc
Goutam Singh <java.lang.goutam@gmail.com>
2024-05-08 18:07:22 +0530

    Time: 5 ms (97.00%), Space: 45.3 MB (92.42%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.java
+++ 0506-relative-ranks/0506-relative-ranks.java
@@ -1,24 +1,34 @@
 class Solution {
 
+    public int[] getHashTable(final int[] score) {
+        int max = -1;
+        for (int sc : score) {
+            if (max < sc) max = sc;
+        }
+        
+        return new int[max + 1];
+    }
+    
     public String[] findRelativeRanks(int[] score) {
-        final Map<Integer, Integer> map = new HashMap<>();
+        
+        final int[] hashTable = getHashTable(score);
         final int n = score.length;
-
+        
         for (int i = 0; i < n; i++) {
-            map.put(score[i], i);
+            hashTable[score[i]] = i;
         }
 
         Arrays.sort(score);
 
         final String[] result = new String[n];
 
-        if (n >= 1) result[map.get(score[n - 1])] = "Gold Medal";
-        if (n >= 2) result[map.get(score[n - 2])] = "Silver Medal";
-        if (n >= 3) result[map.get(score[n - 3])] = "Bronze Medal";
+        if (n >= 1) result[hashTable[score[n - 1]]] = "Gold Medal";
+        if (n >= 2) result[hashTable[score[n - 2]]] = "Silver Medal";
+        if (n >= 3) result[hashTable[score[n - 3]]] = "Bronze Medal";
 
         int pos = 4;
         for (int i = n - 4; i >= 0; i--) {
-            result[map.get(score[i])] = String.valueOf(pos++);
+            result[hashTable[score[i]]] = String.valueOf(pos++);
         }
 
         return result;
