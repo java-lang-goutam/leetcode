0a67def4e76f22529bbcfe5a8af1dd45995044b3
Goutam Singh <java.lang.goutam@gmail.com>
2024-05-12 21:06:01 +0530

    Time: 3 ms (62.99%), Space: 44.9 MB (96.71%) - LeetHub

--- 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.java
+++ 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.java
@@ -6,15 +6,11 @@ class Solution {
 
         for (int i = 0; i < n - 2; i++) {
             for (int j = 0; j < n - 2; j++) {
-                
-                int maxVal = 0;
-                for (int r = 0; r < 3; r++) {
-                    for (int c = 0; c < 3; c++) {
-                        maxVal = Math.max(maxVal, grid[i + r][j + c]);
+                for (int r = i; r < i + 3; r++) {
+                    for (int c = j; c < j + 3; c++) {
+                        res[i][j] = Math.max(res[i][j], grid[r][c]);
                     }
                 }
-
-                res[i][j] = maxVal;
             }
         }
 
