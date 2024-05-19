7f852e0570fe5c7032d7736d0c8def683cf5464e
Goutam Singh <java.lang.goutam@gmail.com>
2024-05-19 17:12:23 +0530

    Time: 7 ms (41.22%), Space: 55.2 MB (29.77%) - LeetHub

---
+++ 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.java
@@ -0,0 +1,37 @@
+/**
+https://www.youtube.com/watch?v=bnBp6_b4GCw
+*/
+
+class Solution {
+    public long maximumValueSum(int[] nums, int k, int[][] edges) {
+        int n = nums.length;
+        int[] netChange = new int[n];
+        long nodeSum = 0;
+
+        for (int i = 0; i < n; i++) {
+            netChange[i] = (nums[i] ^ k) - nums[i];
+            nodeSum += nums[i];
+        }
+
+        Arrays.sort(netChange);
+        // Reverse the sorted array
+        for (int i = 0; i < n / 2; i++) {
+            int temp = netChange[i];
+            netChange[i] = netChange[n - 1 - i];
+            netChange[n - 1 - i] = temp;
+        }
+
+        for (int i = 0; i < n; i += 2) {
+            // If netChange contains odd number of elements break the loop
+            if (i + 1 == n) {
+                break;
+            }
+            long pairSum = netChange[i] + netChange[i + 1];
+            // Include in nodeSum if pairSum is positive
+            if (pairSum > 0) {
+                nodeSum += pairSum;
+            }
+        }
+        return nodeSum;
+    }
+}
