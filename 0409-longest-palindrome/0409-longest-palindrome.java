41afc0550f2c08fa6e83f58035fac3e0630eeb0e
Goutam Singh <java.lang.goutam@gmail.com>
2024-06-04 21:01:09 +0530

    Time: 2 ms (86.01%), Space: 41.5 MB (82.92%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -6,8 +6,9 @@ class Solution {
         for (int i=0, n = s.length(); i < n; i++) {
             final char c = s.charAt(i);
             final int index;
-            if (c <='Z') index = c - 'A';
-            else index = 26 + c - 'a';
+            if (c > 'Z') index = 26 + c - 'a';
+            else index = c - 'A';
+
             if ((++freq[index] & 1) == 0) result += 2;
         }
         
