021b5ae0f6d41934968369e53b921ce91325e78c
Goutam Singh <java.lang.goutam@gmail.com>
2024-06-04 20:57:02 +0530

    Time: 2 ms (86.01%), Space: 41.4 MB (90.37%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -2,16 +2,13 @@ class Solution {
     public int longestPalindrome(String s) {
         
         final int[] freq = new int[52];
+        int result = 0;
         for (int i=0, n = s.length(); i < n; i++) {
             final char c = s.charAt(i);
-            if (c <='Z') freq[c - 'A']++;
-            else freq[26 + c - 'a']++;
-        }
-        
-        int result = 0;
-        for (int i=0; i < 52; i++) {
-            result += freq[i];
-            if ((freq[i] & 1) == 1) result--;
+            final int index;
+            if (c <='Z') index = c - 'A';
+            else index = 26 + c - 'a';
+            if ((++freq[index] & 1) == 0) result += 2;
         }
         
         return result == s.length() ? result : result + 1;
